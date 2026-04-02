a203a7139b6b7eda1825f1247bff1fb43d5eb857
Jonghyun <zxc159285@gmail.com>
2026-04-02 13:07:26 +0900

    0402

---
+++ src/main/java/com/karina/app/Kosomo163BootApplication.java
@@ -0,0 +1,13 @@
+package com.karina.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosomo163BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosomo163BootApplication.class, args);
+	}
+
+}
